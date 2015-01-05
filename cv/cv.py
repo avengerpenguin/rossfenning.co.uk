fa461cd0b199d38fefe660b4570126141d9809fa
Ross Fenning <fennir01@mc-s083545.local>
2015-01-05 00:56:19 +0000

    Got Jinja2 playing nicely with LaTeX.

--- cv/cv.py
+++ cv/cv.py
@@ -2,12 +2,21 @@ from pelican import signals
 from jinja2 import Template
 from laconia import ThingFactory
 from rdflib import Graph
+from jinja2 import Environment, PackageLoader
+
+
+env = Environment(
+    loader=PackageLoader('cv', 'templates'),
+    block_start_string='<%', block_end_string='%>',
+    variable_start_string='<=', variable_end_string='=>')
+
+template = env.get_template('cv.tmpl')
 
 
 def render(sender):
     g = Graph()
     g.parse('cv/cv.ttl', format='turtle')
-    out = Template(open('./cv/cv.tmpl').read()).render(cv=ThingFactory(g)('http://rossfenning.co.uk/cv'))
+    out = template.render(cv=ThingFactory(g)('http://rossfenning.co.uk/cv'))
     print(out.encode('utf-8'))
 
 
