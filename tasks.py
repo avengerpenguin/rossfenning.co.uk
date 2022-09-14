2600699cb9c8a09dca4e159e49967d53e13c03ca
Ross Fenning <ross.fenning@bbc.co.uk>
2022-09-14 13:34:15 +0100

    Bring over improvements for Python roles

--- tasks.py
+++ tasks.py
@@ -2,13 +2,3 @@ import voltaire
 from invoke import task
 
 namespace = voltaire.site()
-
-
-@task
-def cv_forks(c):
-    with c.cd("content/extra"):
-        c.run("xelatex -halt-on-error -interaction=batchmode python.tex")
-
-
-namespace.add_task(cv_forks)
-namespace["build"].pre.append(cv_forks)
