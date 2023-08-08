06b5416f122b0880c222c20550c0aafa5dad1478
Ross Fenning <ross.fenning@inrix.com>
2023-08-08 20:27:29 +0100

    Add favicon

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,10 +13,19 @@ PLUGINS += [
     # "projects",
 ]
 
-STATIC_PATHS = ["extra/cv.pdf", "extra/python.pdf", "papers"]
+STATIC_PATHS = ["extra/cv.pdf", "papers", "../icon"]
 EXTRA_PATH_METADATA = {
     "extra/cv.pdf": {"path": "cv.pdf"},
-    "extra/python.pdf": {"path": "python.pdf"},
+    "../icon/android-chrome-512x512.png": {"path": "android-chrome-512x512.png"},
+    "../icon/android-chrome-192x192.png": {"path": "android-chrome-192x192.png"},
+    "../icon/apple-touch-icon.png": {"path": "apple-touch-icon.png"},
+    "../icon/favicon-16x16.png": {"path": "favicon-16x16.png"},
+    "../icon/favicon-32x32.png": {"path": "favicon-32x32.png"},
+    "../icon/mstile-150x150.png": {"path": "mstile-150x150.png"},
+    "../icon/favicon.ico": {"path": "favicon.ico"},
+    "../icon/safari-pinned-tab.svg": {"path": "safari-pinned-tab.svg"},
+    "../icon/browserconfig.xml": {"path": "browserconfig.xml"},
+    "../icon/site.webmanifest": {"path": "site.webmanifest"},
 }
 
 TEMPLATE_PAGES = {
